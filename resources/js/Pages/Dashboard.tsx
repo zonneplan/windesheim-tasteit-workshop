3362ff7fdeab8d6cf5bee94567374c1957257387
Maarten Bode <maarten.bode@zonneplan.nl>
2024-05-14 15:19:43 +0200

    Add dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,13 +1,41 @@
 import Layout from "../Layouts/Layout";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { DateTime } from "luxon";
 
 export default function Dashboard({ prices }: PageProps) {
     return (
         <Layout>
             <Head title="Dashboard" />
 
-            <p>Hier komt je dashboard...</p>
+            <div>
+                <div className="flex justify-between font-bold mb-3">
+                    <span>Tijdstip</span>
+                    <span>Prijs</span>
+                </div>
+
+                <div className="grid gap-y-1">
+                    {prices.map((price) => (
+                        <div
+                            key={price.dateTime}
+                            className="flex justify-between"
+                        >
+                            <span>
+                                {DateTime.fromISO(price.dateTime).toFormat(
+                                    "cccc t",
+                                )}
+                            </span>
+
+                            <span>
+                                &euro;{" "}
+                                {(price.priceTotalTaxIncluded / 100000).toFixed(
+                                    3,
+                                )}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </Layout>
     );
 }
